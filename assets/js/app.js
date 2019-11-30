d79a0db68c411d7b1ab2b1c93279c210252256de
LMBoyle <boylelukem@gmail.com>
2019-11-30 12:13:35 -0500

    styleing on random title and fill in on click

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,13 @@
 // Vars ===========================================================================================
 
-var key = secrets.API_KEY;
-var wordUrl = 'https://random-word-api.herokuapp.com/word?key=' + key + '&number=3';
-console.log(wordUrl)
+var 
+  key = secrets.API_KEY,
+  wordUrl = 'https://random-word-api.herokuapp.com/word?key=' + key + '&number=3';
+
+const
+  wordOne = $('#wordOne'),
+  wordTwo = $('#wordTwo'),
+  wordThree = $('#wordThree');
 
 // Functions ======================================================================================
 
@@ -11,9 +16,14 @@ var generate = () => {
   $.ajax({
     url: wordUrl,
     method: 'GET'
-  }).then(function(response) {
-    console.log(response);
+  }).then(function(res) {
+    // TODO If returns 'wrong api key'
+    console.log(res);
+    wordOne.empty().append(res[0]);
+    wordTwo.empty().append(res[1]);
+    wordThree.empty().append(res[2]);
   });
+
 };
 
 
